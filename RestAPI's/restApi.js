63d5450aa05f1e57844f1d40d1a80726cabcc5f1
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-06-24 12:48:35 +0530

    Update restApi.js

--- RestAPI's/restApi.js
+++ RestAPI's/restApi.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const port = 4000;
 const users = require('./MOCK_DATA.json');
 const app = express();
+app.use(express.urlencoded({extended: false})); // middleware : express dont know the type of body generated.
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Homepage!");
@@ -24,18 +28,60 @@ app.get('/api/users', (req, res) => {
 // now suppose we want to access the user by its :id, let's see how to handle it;
 app.route('/api/users/:id').get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
+    const userIndex = users.findIndex((user) => user.id === id);
+    if(userIndex === -1){
+        return res.status(404).json({status : "user not found!"});
+    }else{
+        return res.json(users[userIndex]);
+    }
 
 }).patch((req, res) => {
-
-    //TODO: Edit new USER
-    return res.json({staus:"pending"});
+    const id = Number(req.params.id);
+    const body = req.body;
+    let userIndex = users.findIndex((user) => user.id === id);
+    if(userIndex !== -1){
+        users[userIndex] = {...users[userIndex], ...body};
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+            if(err){
+                return res.json({status : "pending"});
+            }else{
+                return res.json({status : "success", id : id});
+            }
+        })
+    }else{
+        return res.status(404).json({status : "user not found!"});
+    }
 }).delete((req, res) => {
-
-    //TODO: Delete a user with its id
-    return res.json({staus:"pending"});
+    const id = Number(req.params.id);
+    let userIndex = users.findIndex((user) => user.id === id);
+    if(userIndex !== -1){
+        users.splice(userIndex, 1);
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.json({ status: "pending" });
+            } else {
+                return res.json({ status: "success", id: id });
+            }
+        });
+    }else{
+        return res.status(404).json({status: "user not found!"});
+    }
 });
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    const newUser = {...body, id: users.length + 1};
+    users.push(newUser);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+            return res.json({status: "pending"});
+        } else {
+            return res.json({status: "success", id: newUser.id});
+        }
+    });
+});
+
 
 app.listen(port, ()=>{
     console.log(`server is listening on port ${port}.`);
