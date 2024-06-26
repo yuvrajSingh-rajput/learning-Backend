b2f627852a9606df6d06eda89643b6ca82412f20
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-06-27 02:05:03 +0530

    Create index.js

---
+++ MODEL VIEWS ROUTES CONTROLLER and MIDDLEWARES/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const port = 4000;
+const userRouter = require('./routes/user');
+const {connectMongoDB} = require('./connection');
+const {logReqRes} = require('./middlewares');
+const app = express();
+
+connectMongoDB('mongodb://127.0.0.1:27017/you-tube-clone').then(() =>console.log('database connected successfully!')).catch((err) => console.log('error in connecting database ', err));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//ROUTES
+app.use('/user', userRouter);
+// MIDDLEWARE
+app.use(logReqRes('log.txt'));
+
+app.listen(port, ()=>{
+    console.log(`server is listening on http://localhost:${port}`);
+});
