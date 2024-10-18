855e86d94a37913904ced103ec358b6d13d1b00b
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-10-19 02:32:29 +0530

    Update index.ts

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -27,7 +27,7 @@ if(isValid(15)) console.log("user is valid!");
 else console.log("User is not valid!");
 
 // question 5;
-function runAfter1s(fn : () => void){   // return typeof function
+function runAfter1s(fn : (a: number, b: number) => void){   // return typeof function
     setTimeout(fn, 1000);
 }
 function run(a: number, b: number){
@@ -96,7 +96,7 @@ class Employee implements Person{
 const e = new Employee("yuvraj", 22);
 console.log(e.greet("Welcome back"));
 
-//TODO: UNIONS: (can not be done using interfaces)
+//TODO: ----------UNIONS: (can not be done using interfaces) L = R1 | R2 then => L: type(only) while Ri(type or interfaces)-----------
 // Question 8: print the id of a user which can be either number or string
 type typeId = string | number;
 function getId(id: (string | number)){
@@ -105,7 +105,7 @@ function getId(id: (string | number)){
 getId(6846181729);
 getId("fhoqwj382973qkl23c--=p");
 
-//TODO: INTERSECTION: (can not be done using interfaces)
+//TODO: --------INTERSECTION: (can not be done using interfaces) L = R1 | R2 then => L: type(only) while Ri(type or interfaces)--------
 type Employees = {
     name: string,
     startDate: Date;
@@ -115,8 +115,62 @@ type Managers = {
     department: string;
 };
 type TamLead = Employees & Managers;     // Employee as well as Manager
-const teamLead: TamLead = {
+const teamLead: TamLead ={
     name: "yuvraj singh",
     startDate: new Date(),
     department: "Computer Science"
 };
+
+// Question 9: filter out the users that are legal
+interface User2 {
+    firstname: string,
+    lastname: string,
+    age: number;
+}
+function filterUsers(users: User2[]){
+    const result = users.filter((user) => {
+        user.age >= 18;
+    });
+    return result;
+}
+
+//! Note: We can not define multiple functions of same name in same directory either are in different files too in typescript in case they are not exported
+
+//TODO: ---------------------------------------------------------ENUMS-------------------------------------------------------------------
+enum Directions {
+    Up = 1,
+    Down, // By default 2
+    Left, // 3
+    Right // 4
+}
+function doSomething(keyPressed: Directions){
+    if(keyPressed == Directions.Down){
+        // do something required
+    }
+}
+doSomething(Directions.Up);
+doSomething(Directions.Down);
+console.log(Directions.Up);
+console.log(Directions.Down);
+console.log(Directions.Left);
+console.log(Directions.Right);
+// one use case in express is to store the status codes as enums and return as status in more readable format
+
+
+//TODO: ------------------------------------------------------------GENERICS-----------------------------------------------------------
+function identity<T>(arg: T){
+    return arg;
+}
+const output1 = identity<string>("yuvraj");
+const output2 = identity<number>(10);
+// now we can do 
+output1.toUpperCase();
+
+// Question 10: Write a function that takes an array as input of a type and if it is string array the convert all the strings in upppercase
+function convertUppercase<T>(arr: T[]): T{
+    return arr[0];
+}
+const output = convertUppercase<string>(["yuvraj singh", "shiva singh"]);
+console.log(output.toUpperCase());
+const output3 = convertUppercase<number>([1, 2, 3]);
+console.log(output3);
