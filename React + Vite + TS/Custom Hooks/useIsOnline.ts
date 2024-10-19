0ef37020d3777d108171d0bf9001907ff2174141
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-10-20 01:33:17 +0530

    Add files via upload

---
+++ React + Vite + TS/Custom Hooks/useIsOnline.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useIsOnline(){
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setIsOnline(true);
+    });
+    window.addEventListener("offline", () => {
+      setIsOnline(false);
+    });
+  }, []);
+
+  return isOnline;
+}
